8c663304ba2187ea75ba1fa1a1ab2fdc4b3f8e0e
Dayeon myeong <meme91322367@gmail.com>
2021-06-07 14:19:16 +0900

    Update profile.js

--- routes/api/creator/profile/profile.js
+++ routes/api/creator/profile/profile.js
@@ -15,16 +15,6 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 
-//4. 크리에이터 프로필 조회 okdk
-//크리에이터명 프로필사진 크리에이터설명 카테고리(먹방2위)  즐겨찾는유저(팬게시판 좋아요) 구독자 누적조회수 
-//랭크 등급, 업적등급
-//팬게시판 여부
-//랭킹 경험치, 업적 경험치(마스터일땐 경험치 없다)
-//랭킹 이미지,업적 이미지! 등급 이미지!!
-//순위
-//카테고리 먹방 2위!!
-
-
 router.get('/video/crawl', async(req, res) => {
 
 
@@ -45,9 +35,6 @@ router.get('/video/crawl', async(req, res) => {
         const ans = JSON.parse(JSON.stringify(getPostByHotResult[0]));
         const ansss = [];
 
-        console.log(ans[0].channel_id);
-
-//        console.log(getPostByHotResult[0][0].channel_id);
         for(var i = 0;i<ans.length;i++){
             ansss[i] = ans[i].channel_id;
 
@@ -74,47 +61,30 @@ router.get('/video/crawl', async(req, res) => {
 
 
 router.post('/video/crawlHotinput', async(req, res) => {
-//6열
       var dataArray= fs.readFileSync('hotoutput.csv', 'utf8').toString().split("\n");
-    //var dataArray = data.split("\n"); //Be careful if you are in a \r\n world... 
-    // Your array contains ['ID', 'D11', ... ] 
-
-//for문
+  
     for(var i = 0;i<dataArray.length;i++){
         strArray = dataArray[i].split(',');
 
-        console.log("strArray");
-        console.log(strArray);
-        console.log("dataArray");
-        console.log(dataArray[i]);
         let getChannel_id = strArray[0] || null;
         let getTitle  = strArray[1] || null;
         let getVideoLink = strArray[2] || null;
         let getTime = strArray[3] || null;
         let getViewCnt = strArray[4] || null;
         let getThumbnailImg  = strArray[5] || null;
-
-
-
+        
         if(!getChannel_id || !getTitle || !getVideoLink){
             continue;
         }else{
             const getCreatorIdxQuery= `SELECT idx FROM creator where channel_id=?`;  
-
             var getCreatorIdxResult  = await db.queryParam_Parse(getCreatorIdxQuery,[strArray[0]]);
-            console.log("getchannelId");
-            
-            
-
-            
-        //hotcsv파일            
-
+        
             if(!getCreatorIdxResult || getCreatorIdxResult.length == 0){
                 console.log("creator_idx");
                 console.log(getCreatorIdxResult);
             }else{
                 var creator_idx = JSON.parse(JSON.stringify(getCreatorIdxResult[0][0])).idx;
-                console.log(creator_idx);//4659
+
                 const params = [creator_idx,getTitle,getVideoLink,getViewCnt,getThumbnailImg,getTime];
                 const insertCreatorHotVideoQuery = "INSERT INTO video(creator_idx, title, video_url, view_cnt, thumbnail_url, create_time,if_hot,if_new,insert_time)\
                 VALUES(?,?,?,?,?,?,1,0, now())";
@@ -140,18 +110,12 @@ router.post('/video/crawlHotinput', async(req, res) => {
 
 
 router.post('/video/crawlNewinput', async(req, res) => {
-//6열
+
     var dataArray= fs.readFileSync('newoutput.csv', 'utf8').toString().split("\n");
-    //var dataArray = data.split("\n"); //Be careful if you are in a \r\n world... 
-    // Your array contains ['ID', 'D11', ... ] 
-//for문
+    
     for(var i = 0;i<dataArray.length;i++){
         strArray = dataArray[i].split(',');
 
-        console.log("strArray");
-        console.log(strArray);
-        console.log("dataArray");
-        console.log(dataArray[i]);
         let getChannel_id = strArray[0] || null;
         let getTitle  = strArray[1] || null;
         let getVideoLink = strArray[2] || null;
@@ -163,14 +127,8 @@ router.post('/video/crawlNewinput', async(req, res) => {
             continue;
         }else{
             const getCreatorIdxQuery= `SELECT idx FROM creator where channel_id=?`;  
-
             var getCreatorIdxResult  = await db.queryParam_Parse(getCreatorIdxQuery,[strArray[0]]);
-            console.log("getchannelId");
-            
-            
-
-            
-        //hotcsv파일            
+               
 
             if(!getCreatorIdxResult || getCreatorIdxResult.length == 0){
                 console.log("creator_idx");
@@ -199,18 +157,6 @@ router.post('/video/crawlNewinput', async(req, res) => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 router.get('/:creatorIdx', async (req, res) => {
     const { creatorIdx } = req.params;
 
@@ -230,26 +176,16 @@ b.idx AS 'board_idx' ,b.name AS 'board_name',b.type AS 'board_type'
     WHERE c.idx = ?`;
     const getCreatorProfileResult = await db.queryParam_Parse(getCreatorProfileQuery, [creatorIdx]);
 
-//    console.log(getCreatorProfileResult);
-//    const result = getCreatorProfileResult[0];
-
-//console.log(getCreatorProfileResult[0]);
-
-//console.log(getCreatorProfileResult);
     
     let flev = 0;
     if (!getCreatorProfileResult) {
-        console.log("543");
         res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.CREATOR_SELECT_PROFILE_ERROR));
     } else if(getCreatorProfileResult[0].length === 0){
-        console.log("2352");
         res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.CREATOR_SELECT_PROFILE_NOTHING));
     }
     else{
-//        res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.CREATOR_SELECT_PROFILE_SUCCESS, getCreatorPrifileResult));
         
         const result = JSON.parse(JSON.stringify(getCreatorProfileResult[0][0]));
-    console.log(result);
 
     const category_json = JSON.parse(JSON.stringify(getCreatorProfileResult));
     const categoryIdx = category_json[0][0].category_idx;
@@ -266,106 +202,75 @@ b.idx AS 'board_idx' ,b.name AS 'board_name',b.type AS 'board_type'
 
         switch (follower_grade_name){
         case  "브론즈":
-        console.log("66");
         flev = (youtube_subscriber_cnt - follower_cnt)/200*100;
             result["back_lank_exp"] = follower_cnt+99;
             break;
         case "실버" :
-        console.log("55");
             result["back_lank_exp"] = follower_cnt+999;
             flev = (youtube_subscriber_cnt - follower_cnt)/2000*100;
             break;
         case "골드":
-        console.log("44");
         flev = (youtube_subscriber_cnt - follower_cnt)/20000*100;
             result["back_lank_exp"] = follower_cnt+9999;
             break;
         case "플레티넘":
-        console.log("33");
             result["back_lank_exp"] = follower_cnt+99999;
             flev = (youtube_subscriber_cnt - follower_cnt)/200000*100;
             break;
         case "다이아" :
-            console.log("22");
             result["back_lank_exp"] = follower_cnt+999999;
              flev = (youtube_subscriber_cnt - follower_cnt)/2000000*100;
             break;
         case "마스터":
-        console.log("11");
             flev = 0;
             result["back_lank_exp"] = 0;
         
     }
 
-    console.log("flevss");
-    console.log(flev);
 
     let flex = 0;
     switch (view_grade_name){
         case  "F":
-        console.log("f");
             flex = youtube_view_cnt/100000*100;
             result["back_lank2_exp"] = 100000;
             break;
         case "D" :
-        console.log("d");
             flex = youtube_view_cnt/900000*100;
             result["back_lank2_exp"] = 1000000;
             break;
         case "C":
-        console.log("c");
             flex = youtube_view_cnt/9000000*100;
             result["back_lank2_exp"] = 10000000;
             break;
         case "B":
-        console.log("b");
             flex = youtube_view_cnt/90000000*100;
             result["back_lank2_exp"] = 10000000;
             break;
 
         case "A":
-        console.log("a");
             flex = youtube_view_cnt/900000000*100;
             result["back_lank2_exp"] = 100000000;
             break;
         case "S":
-        console.log("s");
             flex = 0;
             result["back_lank2_exp"] = 0;
            
     }
-        console.log("flex");
-        console.log(flex);
 
-
-            //1등급이면 *2
             if(follower_grade_level  === 1){
                 flev = flev*2;
             }
 
-            //flev /= 10000;
-            console.log("flev");
-            console.log(flev);
             result["front_lank_exp"]=youtube_subscriber_cnt;
             result["front_lank2_exp"]=youtube_view_cnt;
             result["follower_grade_percent"]= parseInt(flev);
             
             result["view_grade_percent"]=parseInt(flex);
-  //특정 카테고리별 크리에이터들 리스트
     const getCreatorsQuery=`SELECT cc.creator_idx ,c.*
     FROM creator_category cc 
     INNER JOIN creator c ON c.idx = cc.creator_idx 
     WHERE cc.category_idx = ?
     ORDER BY c.youtube_subscriber_cnt DESC`;
-
-/*
-#순위리스트
-SELECT cc.creator_idx ,c.*
-    FROM creator_category cc 
-    INNER JOIN creator c ON c.idx = cc.creator_idx 
-    WHERE cc.category_idx = 5
-    ORDER BY c.youtube_subscriber_cnt DESC;
-    */
 
     const getCreatorsResult = await db.queryParam_Parse(getCreatorsQuery,categoryIdx);
     const creators_json = JSON.parse(JSON.stringify(getCreatorsResult));
@@ -404,18 +309,6 @@ SELECT cc.creator_idx ,c.*
 
 
 
-
-
-
-
-//크리에이터 프로필의 스탯조회 okdk
-/*    
-SELECT cs.score,s.name
-FROM creator_stat cs
-INNER JOIN stat s ON s.idx = cs.stat_idx
-WHERE cs.creator_idx = 1717;
-
-*/
 router.get('/stat/:creatorIdx', async (req, res) => {
     const { creatorIdx } = req.params;
 
@@ -434,7 +327,6 @@ GROUP BY cs.stat_idx`;
         res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.CREATOR_SELECT_STAT_ERROR));
     } else {
 
-        //avg 쿼리
         const getCreatorStatAvgQuery = `SELECT AVG(cs.score) AS 'avg_stat',
         COUNT(cs.score) AS 'join_cnt_stat'
 FROM creator_stat cs
@@ -456,13 +348,9 @@ WHERE cs.creator_idx = ?`;
     }
 });
 
-
-
-//크리에이터 프로필의 스탯 등록창 조회 okdk
 router.get('/stat/detail/:creatorIdx', async (req, res) => {
     const { creatorIdx } = req.params;
-//소통 매력 실력 고막 개성
-//설명
+
     let getStatDetailQuery  = `SELECT DISTINCT s.*
 FROM creator_stat cs
 INNER JOIN stat s ON s.idx = cs.stat_idx
@@ -470,18 +358,15 @@ WHERE cs.creator_idx = ?`;
     
     const getStatDetailResult = await db.queryParam_Parse(getStatDetailQuery,[creatorIdx]);
 
-    //쿼리문의 결과가 실패이면 null을 반환한다
-    if (!getStatDetailResult) { //쿼리문이 실패했을 때
+    if (!getStatDetailResult) { 
         res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.CREATOR_SELECT_STAT_DETAIL_ERROR));
     } else if(getStatDetailResult.length === 0){
         res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.CREATOR_SELECT_STAT_DETAIL_ERROR));
-    }else{ //쿼리문이 성공했을 때
+    }else{ 
         res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.CREATOR_SELECT_STAT_DETAIL_SUCCESS,getStatDetailResult[0]));
     }
 });
 
-
-//크리에이터 프로필의 스탯 등록 okdk
 router.post('/stat/detail/:creatorIdx', authUtil.isLoggedin, async(req, res) => {
     
     let statIdx = [];
@@ -524,22 +409,14 @@ router.post('/stat/detail/:creatorIdx', authUtil.isLoggedin, async(req, res) => 
     });
 
 
-    
-
-
-
-
-
-
-
-//해쉬태그 등록 okdk
+  
 router.post('/stat/hashtag/:creatorIdx', authUtil.isLoggedin, async(req, res) => {
     
 
     const creatorIdx = parseInt(req.params.creatorIdx);
     const userIdx = req.decoded.user_idx;
     const hashtagName = req.body.hashtagName;
-    //해쉬태그 있는지
+
     const gethashtagQuery = "SELECT idx FROM hashtag WHERE name = ?";
     const gethashtagResult = await db.queryParam_Parse(gethashtagQuery, [hashtagName]);
 
@@ -552,7 +429,7 @@ router.post('/stat/hashtag/:creatorIdx', authUtil.isLoggedin, async(req, res) =>
     }
         
 
-     if(!hashtagIdx || !gethashtagResult[0][0]){//해쉬태그 없는 경우 //해쉬 태그 
+     if(!hashtagIdx || !gethashtagResult[0][0]){
 
         const insertHashtagQuery = "INSERT INTO hashtag(name) VALUES(?)";
         const insertHashtagResult = await db.queryParam_Parse(insertHashtagQuery,[hashtagName]);
@@ -577,7 +454,7 @@ router.post('/stat/hashtag/:creatorIdx', authUtil.isLoggedin, async(req, res) =>
 
         }
             
-    }else{//해쉬태그 있는 경우 //creator_hashtag에만 등록
+    }else{
         const insertStatHashtagQuery = "INSERT INTO creator_hashtag(creator_idx, hashtag_idx) VALUES(?, ?)";
         const insertStatHashtagResult = await db.queryParam_Parse(insertStatHashtagQuery,[creatorIdx,hashtagIdx]);
 
@@ -593,9 +470,6 @@ router.post('/stat/hashtag/:creatorIdx', authUtil.isLoggedin, async(req, res) =>
     }
     
 });
-
-
-
 
 
 module.exports = router;
